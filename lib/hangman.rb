a28648e0e6f83a908be1f8f51a57756f67db28f7
Xue1126 <tanxue1126@gmail.com>
2017-03-25 11:01:35 +0100

    checking letters

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,30 +6,33 @@ class Hangman
     # @bad_guesses_left = 10
     @guesesses_positions = []
     @bad_guesses_list = []
-    for i in 0 .. @random_word.word_length do
+    for i in 0 ... @random_word.word_length do
       @guesesses_positions <<  "_"
     end
     # print @correct_guesses.join(" ") + "\n"
   end
 
+  # this function plays the game
   def play
-    puts "Hi from lib/hangman.rb!"
+    # puts "Hi from lib/hangman.rb!"
     show_game
     letter = ask_letter
     check_letter(letter)
   end
 
-  # puts ["red", "blue", "yellow"].join(", ")
+  # this shows the current state of the game on the terminal
+  ## puts ["red", "blue", "yellow"].join(", ")
   def show_game
     puts @guesesses_positions.join(" ") + "\n"
     puts @bad_guesses_list.join(" ") + "\n"
 
   end
 
+  # keep asking the user for a letter until he chooses an acceptable one
   def ask_letter
     is_valid = false
     while !is_valid
-      print "please enter a letter... "
+      print 'please enter a letter... '
       user_input = gets.chomp.downcase
       if user_input.length == 1
         if user_input =~ /[a-z]/
@@ -40,7 +43,12 @@ class Hangman
     end
     return user_input
   end
+
+  # check whether a letter is correct (and where) or incorrect
   def check_letter(letterx)
-    puts "checking letter" , letterx
+    # puts 'checking letter', letterx
+    is_correct = false
+    positions = @random_word.positions_for(letterx)
+    print positions
   end
 end
--- lib/random_word.rb
+++ lib/random_word.rb
@@ -1,8 +1,10 @@
 class RandomWord
   attr_reader :word
 
-  WORDS = %w(programming monkeybusiness rubyesque styleguide)
+  # WORDS = %w(programming monkeybusiness rubyesque styleguide kip)
+  WORDS = %w(kippen)
 
+  # choose one random word from the list
   def initialize
     @word = WORDS.sample
   end
@@ -11,7 +13,18 @@ class RandomWord
     return @word.length
   end
 
-  def positions_for(characters: [])
-    # for you to implement :)
+  # this function will find the letter position in the secret word, for example
+  # if letter is 'a' and the secret word is 'calendar' then result is [1, 6]
+  def positions_for(inletter)
+    positions = []
+    for current_letter_pos in 0 ... word_length
+      wordletter = @word[current_letter_pos]
+      # puts "#{i}: #{wordletter}"
+      if inletter == wordletter
+        # puts 'yaaay'
+        positions << current_letter_pos
+      end
+    end
+  return positions
   end
 end
